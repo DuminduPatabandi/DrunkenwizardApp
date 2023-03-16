00f0254f57340205cb7f79962020ee430d4bc9bc
DuminduPatabandi <dppatabandi@gmail.com>
2023-03-16 21:40:07 +0530

    Sneek a peak added

--- drunked/src/pages/Gallery.jsx
+++ drunked/src/pages/Gallery.jsx
@@ -1,12 +1,14 @@
 import { mygallery } from '../constants'
 import styles from '../style'
+import { Sneek } from '../components'
+
 
 export default function Gallery() {
   return (
     <section id='work '>
       <div className="bg-[#f9f9f9]">
         <div className="mx-auto max-w-full py-6 px-20 sm:py-14">
-          <h2 className={`${styles.paddingNew} font-shadows text-[#23221f] text-[54px] tracking-[19.2px] mb-10 text-center`}>Gallery</h2>
+          <h2 className={`${styles.paddingNew} font-shadows text-[#23221f] text-[54px] tracking-[19.2px] mt-6 mb-10 text-center`}>Gallery</h2>
           <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-3 lg:grid-cols-4  xl:gap-x-8">
             {mygallery.map((product) => (
                 <div className="aspect-w-1  duration-700  aspect-h-1 w-full overflow-hidden  xl:aspect-w-7 xl:aspect-h-8">
@@ -19,6 +21,8 @@ export default function Gallery() {
           </div>
         </div>
       </div>
+
+      <Sneek/>
     </section>
   )
 }
--- drunked/src/pages/Video.jsx
+++ drunked/src/pages/Video.jsx
@@ -1,8 +1,18 @@
+import styles from '../style'
+import { Sneek } from '../components'
 
-const Video = () => {
+
+export default function Video() {
   return (
-    <div>Video</div>
+    <section id='work '>
+      <div className="bg-[#f9f9f9]">
+        <div className="mx-auto max-w-full py-6 px-20 sm:py-14">
+          <h2 className={`${styles.paddingNew} font-shadows text-[#23221f] text-[54px] tracking-[19.2px] mt-6 mb-10 text-center`}>Videos</h2>
+          <a className="flex text-center bg-white py-4 px-10 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 duration-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500" target='_blank' href="https://www.shutterstock.com/g/DbrosVmaker">Videos</a>
+        </div>
+      </div>
+
+      <Sneek/>
+    </section>
   )
 }
-
-export default Video
